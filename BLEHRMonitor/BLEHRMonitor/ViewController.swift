b17173da61b03c08268a9e085d6a0ca541a4f760
zukyCoder <zukydeveloper@gmail.com>
2024-09-24 13:03:52 +0200

    create basic swift architecture

---
+++ BLEHRMonitor/BLEHRMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLEHRMonitor
+//
+//  Created by Mario Andres Heberthardt Marchant on 24/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
